4f7bfba0fc824bc80aee47abadb13e25519487f5
Mateustavares10 <mateus-martins10@hotmail.com>
2023-05-16 19:18:29 -0300

    Atividade estruturas condicionais

---
+++ Mateus Tavares/Workspace/JavaScript/Aula04_Estruturas_Condicional/atividade.js
@@ -0,0 +1,10 @@
+let numero1 = 5
+let numero2 = 3
+
+if (numero1 > numero2) {
+    console.log( `O número ${numero1} é maior que ${numero2}` )
+}
+
+else {
+    console.log( `O número ${numero2} é maior que ${numero1}` )
+}
